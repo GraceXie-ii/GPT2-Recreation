858e05eca77a76f7b560a0f246a9f9a951a40395
Grace Xie <gracexielingke@gmail.com>
2025-02-22 00:39:47 -0500

    Something?

--- train_gpt2.py
+++ train_gpt2.py
@@ -163,26 +163,49 @@ class GPT(nn.Module):
 
 # -----------------------------------------------------------------------------
 
-#get a data batch
 import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-buf = buf.to('cpu')
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+
+#Very simple dataloader that goes through our data in chunks
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2') #Tokenizing everything from input.txt
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B*T)} batches") #Going through it in batches of B*T, multiple batches = 1 epochg
+
+        #state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
+        x = (buf[:-1].view(B, T)) # inputs
+        y = (buf[1:]).view(B, T) # targets/labels
+        #advance the position in the tensor
+        self.current_position += B * T
+        #if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
+train_loader = DataLoaderLite(B=4, T=32)
 
 # get logits
 model = GPT(GPTConfig())
 model.to('cpu')
 
-#optimizer !
+#optimize !
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to('cpu'), y.to('cpu')
     optimizer.zero_grad() #Must set to 0 since .backward() will accumulate gradients
     logits, loss = model(x, y)
     loss.backward()
